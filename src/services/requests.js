a6fd2ad5ea97e8867d778cb3c823ea07c41be966
Nauman Majeed <inaumanmajeed@gmail.com>
2024-07-22 01:02:11 +0500

    project finalize

---
+++ src/services/requests.js
@@ -0,0 +1,15 @@
+const API_KEY = "c0cfc208a1459487b5c9400ca4d81d99";
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchPopular: `/movie/popular?api_key=${API_KEY}&language=en-US`,
+    fetchTVShows:`tv/popular?api_key=${API_KEY}&language=en-US&page=1`,
+
+    fetchComedy:`discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchAction:`discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchDocumentaries:`discover/movie?api_key=${API_KEY}&with_genres=99`,
+    fetchHorror:`discover/movie?api_key=${API_KEY}&with_genres=27`,
+    
+}
+
+export default requests;
---
+++ src/utils/commonJson.js
@@ -0,0 +1,31 @@
+import ProfileIcon1 from "../assets/profile-icon-3.svg"
+import ProfileIcon2 from "../assets/profile-icon-3.svg"
+import ProfileIcon3 from "../assets/profile-icon-3.svg"
+import ProfileIcon4 from "../assets/profile-icon-5.svg"
+import ProfileIcon5 from "../assets/profile-icon-5.svg"
+
+const profiles = [
+    {
+        name: "Anna",
+        resource: ProfileIcon1
+    },
+    {
+        name: "user2",
+        resource: ProfileIcon2
+    },
+    {
+        name: "User3",
+        resource: ProfileIcon3
+    },
+    {
+        name: "user4",
+        resource: ProfileIcon4
+    },
+    {
+        name: "User5",
+        resource: ProfileIcon5
+    }
+]
+
+
+export default profiles;
